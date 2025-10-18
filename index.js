420433b891bcf22f08854fa39ac2d799864982e4
Harsh <panwarharsh432@gmail.com>
2025-10-18 19:13:57 +0530

    this is the first commit

--- index.js
+++ index.js
@@ -2,23 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import multer from "multer";
-import path from "path";
-
-import { fileURLToPath } from "url";
-
-
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.set("views", path.join(__dirname, "views"));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
